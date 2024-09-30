fd136e498e74dfab01a826af59e0accee8f030fa
Pavel-tkt <pashankam@gmail.com>
2024-09-30 16:19:42 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,13 @@
+name_of_school="Urban,"
+a=name_of_school
+print(a)
+task_completed=12
+b=task_completed
+print(b)
+time_amount=1.5
+c=time_amount
+print(c)
+name_of_course="Python,"
+d=name_of_course
+print(d)
+print("Школа:",a,"Курс:",d,"всего задач:",b,"затрачено часов:",c,"Среднее время выполнения:",c/b,"часа")
