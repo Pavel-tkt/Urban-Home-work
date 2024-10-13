b537140cc63cf1a89b05a5c1938d0cfd6f23de23
Pavel-tkt <pashankam@gmail.com>
2024-10-13 15:01:55 +0300

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -1,14 +1,21 @@
 my_dict={'Павел':1980, 'Егор':1999, 'Михаил':1890, 'Мария':1939}
-print(my_dict)
+print('Dict:', my_dict)
 print(my_dict['Егор'])
-print(my_dict.get('Лиза'))
+print(my_dict.get('Павел'))
 my_dict.update({'Larisa':2001,
                 'Lili':2020})
 s=my_dict.pop('Мария')
 print(my_dict)
 print(s)
+
 #task2
-my_set={5,9,'i',7,5,'i',7,True,1,1,'rrr'}
+my_set={5,9,'i',3,7,5,'i',7,True,1,1,'rrr'}
 my_set.add('rer')
 my_set.add(14)
-print(my_set)
+print('Set:', my_set)
+print(my_set.remove(3))
+my_set.remove('i')
+print('New_set:',my_set)
+
+
+
