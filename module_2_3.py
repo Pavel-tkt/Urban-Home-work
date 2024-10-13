40bb4411bb5115c520e0af3057b94a335659d937
Pavel-tkt <pashankam@gmail.com>
2024-10-13 15:52:48 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -2,10 +2,15 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 n=0
 while n<len(my_list):
     if my_list[n] >= 0:
-        if my_list[n] ==0:
+        if my_list[n] == 0:
             my_list.remove(0)
+
         print(my_list[n])
-        n = n + 1
+    n= n+1
+    if my_list[n]  < 0:
+        break
+
+
 
 
 
