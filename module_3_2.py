681c772639683c2dcd402fccd025bd53c08b7612
Pavel-tkt <pashankam@gmail.com>
2024-10-24 19:14:00 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -8,15 +8,15 @@ def send_email(message, recipient, sender = 'university.help@gmail.com'):
     elif recipient.endswith(end_variants) and sender.endswith(end_variants):
         if recipient == sender:
             print('Нельзя отправить письмо самому себе!')
-            if sender == 'university.help@gmail.com':
+        elif sender == 'university.help@gmail.com':
                 print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
-            else:
-                print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
         else:
-            print(message)
+                print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+        #else:
+            #print(message)
     else:
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-
+    
 send_email('1Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('2Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('3Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
