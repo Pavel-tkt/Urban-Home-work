6f4216d18f5b00f066a0289e61b57c8751533b8d
Pavel-tkt <pashankam@gmail.com>
2024-10-28 19:58:13 +0300

    Add files via upload

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,16 @@
 #Задача "Рекурсивное умножение цифр":
+from idlelib.replace import replace
+
+
 def get_multiplied_digits(number):
     str_number = str(number)
+    str_number = str_number.replace('0', '') #удаление всех нулей
     first = int(str_number[0])
+
     if len(str_number) > 1:
-         return first * get_multiplied_digits(int(str_number[1:]))
+        return first * get_multiplied_digits(int(str_number[1:]))
+
     else:
-          return first
-result = get_multiplied_digits(40203)
+        return first
+result = get_multiplied_digits(4020300)
 print(result)
