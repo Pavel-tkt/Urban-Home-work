7a3456862ea2efc0c8825008ff2c6c7e50afa212
Pavel-tkt <pashankam@gmail.com>
2024-11-02 14:38:21 +0300

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,9 +1,9 @@
 #Задача "Рассылка писем":
 #Создайте функцию send_email, которая принимает 2 обычных аргумента: сообщение и получатель и
 # 1 обязательно именованный аргумент со значением по умолчанию - отправитель.
-def send_email(message, recipient, sender = 'university.help@gmail.com'):
+def send_email(message, recipient,*, sender = 'university.help@gmail.com'):
     end_variants =('.com', '.ru', '.net')
-    if ("@" not in recipient or "@" not in sender):
+    if ("@" not in recipient and "@" not in sender):
         print('@ expected')
     elif recipient.endswith(end_variants) and sender.endswith(end_variants):
         if recipient == sender:
@@ -16,7 +16,7 @@ def send_email(message, recipient, sender = 'university.help@gmail.com'):
             #print(message)
     else:
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-    
+
 send_email('1Это сообщение для проверки связи', 'vasyok1337@gmail.com')
 send_email('2Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('3Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
