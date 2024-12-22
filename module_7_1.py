afe7a27ab3a27d5471faf02136d4c2263c205c65
Pavel-tkt <pashankam@gmail.com>
2024-12-22 19:03:49 +0300

    Add files via upload

--- module_7_1.py
+++ module_7_1.py
@@ -25,18 +25,20 @@ class Shop:
     #Метод get_products(self), который считывает всю информацию из файла __file_name, закрывает его и возвращает единую строку со всеми товарами из файла __file_name.
     def get_products(self):
         self.file = open(self.__file_name, 'r')
-        return self.file.read()
+        file_info =  self.file.read()
         self.file.close()
+        return file_info
+
 
 
 #Метод add(self, *products), который принимает неограниченное количество объектов класса Product.
 # Добавляет в файл __file_name каждый продукт из products,
 # если его ещё нет в файле (по названию). Если такой продукт уже есть, то не добавляет и выводит строку 'Продукт <название> уже есть в магазине' .
     def add(self, *products):
+        add_new_product = self.get_products()
         for i in products:
-            self.file = open(self.__file_name, 'r')
-            if i.name not in self.file.read():
-                self.file = open(self.__file_name, 'a')
+            self.file = open(self.__file_name, 'a')
+            if i.name not in add_new_product:
                 self.file.write(f'{i}\n')
                 self.file.close()
             else:
